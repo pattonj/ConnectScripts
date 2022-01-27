c4450010d5637d09d90cddda19fac6421d963345
Jacob Patton <jacob@pattons.org>
2022-01-26 20:39:47 -0500

    Update confetti.js

--- confetti.js
+++ confetti.js
@@ -1,41 +1,35 @@
 <script type="module">
-/*Create by Jacob Patton 
-Preview:https://jsfiddle.net/jbongo/2n097rfg/1/
+/*Created by Jacob Patton 
+Preview:https://jsfiddle.net/jbongo/2n097rfg/
 */
 
-//Create a div
-var ConfettiWrapper = document.createElement("div");
-//Set unique ID.
-ConfettiWrapper.id = 'ConfettiWrapper';
-//Set Style and z-index
-ConfettiWrapper.style.cssText='position:absolute;top:0;left:0;min-height:100vh;width:100%';
-ConfettiWrapper.style.zIndex=5;
-
-let i =0;
 //Adjust c for more or less confetti. 
 let c=350;
-//Create the inner HTML for the confetti. 
+//html to add
 let confettiHTML='';
+let confettiArray =[];
+let i =0;
+//create confetti pieces
 for (i; i < c; i++) {
   create(i);
 }
-
-//Set the innerHTML
-ConfettiWrapper.innerHTML = confettiHTML;
-//Append the div/Confetti to the HTML body
-document.body.appendChild(ConfettiWrapper);
+//insert HTML
+document.body.innerHTML += confettiHTML;
 
 i=0; //Reset i to zero to animate
+//Add pieces to array
 for (i; i < c; i++) {
-  drop(i);
+	confettiArray.push(document.getElementById("confetti-"+i));
 }
-
+//drop confetti 
+confettiArray.forEach(drop);
+ 
 //Create the confetti. 
 function create(i) {
   // Set size amd color of confetti piece. 
   let width = Math.random() * 8;
   let height = width * 0.4;
-  //if you want more colors, change the numer here and set the case numbers. 
+  //if you want more colors, change the numer here and set the case numbers the same. 
   let colourIdx = Math.ceil(Math.random() * 4);
   let colour;
   switch(colourIdx) {
@@ -51,22 +45,19 @@ function create(i) {
     default:
       colour = "#C75B12";
   }
-  //Add confetti piece to html section. Creates it way off screen. 
-   confettiHTML +='<div style="position:absolute;background-color:'+colour+';width:'+ width+'px;height:'+ height+'px;top:-200px;left:0;opacity:'+(Math.random()+0.5)+';transform:rotate('+Math.random()*360+'deg);" id="confetti-'+i+'" class="confetti-'+i+' '+colour+'"></div>';
+  //Add confetti piece to html to be inserted. Creates it way off screen. 
+   confettiHTML +='<div style="z-index:3;position:absolute;background-color:'+colour+';width:'+ width+'px;height:'+ height+'px;top:-200px;left:0;opacity:'+(Math.random()+0.5)+';transform:rotate('+Math.random()*360+'deg);" id="confetti-'+i+'" class="confetti-'+i+' '+colour+'"></div>';
 }
 
 //Drop the confetti!
 function drop(i){
-document.getElementById("confetti-"+i).animate(
-   [
-    {top : -Math.random()*80+"%", left : '50%'},//Starts in the middle and slightly above. 
-    {top: '95%', left : Math.random()*95+'%', transform:'rotate('+(Math.random()*2-1)*360+'deg)' } //Stop at the bottom of div and anywhere left to right. 
-  ], Math.random()*2000 + 2000);
-//https://developer.mozilla.org/en-US/docs/Web/API/Element/animate
-}
+	//animate help: https://developer.mozilla.org/en-US/docs/Web/API/Element/animate	
+	i.animate(
+		[
+		{top : -Math.random()*70+"%", left : '50%'},//Starts in the middle and slightly above. 
+		{top: 80+Math.random()*20+'vh', left : Math.random()*95+'%',transform: 'rotate('+(Math.random()*2-1)*360+'deg)' } //Stop at the bottom of div and anywhere left to right. 
+		], Math.random()*2000 + 2000);
+	 i.onfinish = function(i){ i.remove();};
+	 }
+
 </script>
-
-
- 
-
-
